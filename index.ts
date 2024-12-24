ac9675e8deccf8033fc8f8d32a5bb7db7ea6edbd
Mihaela <talmazanmihaela@gmail.com>
2024-12-24 12:49:44 +0100

    Added Task 2

--- index.ts
+++ index.ts
@@ -21,3 +21,53 @@ const animals: Animal[] = [new Dog(), new Cat()];
 animals.forEach((animal) => {
   console.log(animal.makeSound());
 });
+
+// Task 2
+
+abstract class Shape {
+  abstract calculateArea(): number;
+}
+
+abstract class ColoredShape extends Shape {
+  abstract color: string;
+}
+
+class ColoredCircle extends ColoredShape {
+  radius: number;
+  color: string;
+
+  constructor(radius: number, color: string) {
+    super();
+    this.radius = radius;
+    this.color = color;
+  }
+  calculateArea(): number {
+    return Math.PI * this.radius ** 2;
+  }
+}
+
+class ColoredRectangle extends ColoredShape {
+  width: number;
+  height: number;
+  color: string;
+
+  constructor(width: number, height: number, color: string) {
+    super();
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  calculateArea(): number {
+    return this.width * this.height;
+  }
+}
+
+const shapes: ColoredShape[] = [
+  new ColoredCircle(5, "Red"),
+  new ColoredRectangle(3, 4, "Blue"),
+];
+
+shapes.forEach((shape) => {
+  console.log(` Color: ${shape.color}, Area: ${shape.calculateArea()}`);
+});
